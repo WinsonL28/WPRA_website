539d8d955d5e080f3119c27db444292c6561f29a
WinsonL28 <winsonliang@live.com>
2024-06-15 17:29:29 -0700

    Template and file setup

---
+++ scripts/skeleton.js
@@ -0,0 +1,6 @@
+async function setup() {
+    console.log($('#bottom_navbar').load('./text/bottom_nav.html'));
+    console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
+}
+
+setup()
